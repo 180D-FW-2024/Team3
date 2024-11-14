229848b1df215e62d347edead265b92b6bdad01f
Ashish-Basetty <ashish.basetty2@gmail.com>
2024-11-13 17:27:43 -0800

    Started writing communication templates

--- server/server.py
+++ server/server.py
@@ -1,1 +1,9 @@
 # Flask script for handling incoming requests
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+# Initial call to return userID from source IP 
+def start_session():
+    return 
