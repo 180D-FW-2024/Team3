d42fb882316d0c68c11317fdce0f6cb45be2fad8
Brandon Huang <108428038+Gafrindle@users.noreply.github.com>
2024-11-17 22:13:26 -0800

    Standard speech recognition python script

---
+++ sp_recognition.py
@@ -0,0 +1,15 @@
+# Boilerplate code taken from geeksforgeeks.org
+
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Speak")
+    audio_text = r.listen(source)
+    
+    try:
+        # using google speech recognition
+        print("Text: "+r.recognize_google(audio_text))
+    except:
+        print("Error")
