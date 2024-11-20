749f031303b02620ea50f9d09446f44ef2618667
Ashish-Basetty <ashish.basetty2@gmail.com>
2024-11-20 12:19:44 -0800

    Allowed for user creation

--- server/__init__.py
+++ server/__init__.py
@@ -1,10 +1,7 @@
+'''
 from flask import Flask
-import subprocess
-
-subprocess.run(["bash", "FlaskExports.sh"])
-subprocess.run(["python3", "init_db.py"])
-
 app = Flask(__name__)
 
 # Import your routes and models here
 from app import server
+'''
--- server/server.py
+++ server/server.py
@@ -3,6 +3,10 @@ from flask import Flask, jsonify, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .models.userModel import User
+import subprocess
+
+subprocess.run(["bash", "FlaskExports.sh"])
+subprocess.run(["python3", "init_db.py"])
 
 app = Flask(__name__)
 
@@ -14,5 +18,12 @@ session = Session()
 # Initial call to return userID from source IP 
 def start_session(username):
     user = session.query(User).filter_by(username=username).first()
+    
+    if user is None:
+        session.add(User(username=username))
+        session.commit()
+        user = session.query(User).filter_by(username=username).first()
+        return jsonify(user.to_dict()), 201
+    
     return jsonify(user.to_dict()), 200
 
