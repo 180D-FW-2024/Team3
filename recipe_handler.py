2845ccecdd670f027b9c53f4db16acc9f6bec5be
Brandon Huang <108428038+Gafrindle@users.noreply.github.com>
2024-11-25 00:16:49 -0800

    Plan for recipe_handler structure

--- recipe_handler.py
+++ recipe_handler.py
@@ -7,3 +7,6 @@
 # Quantity; 1; Prepare two 9-inch pie crusts and one 9-inch pie dish| Measurement; 150; Measure out 150 grams of white sugar| Measurement; 5.69; Measure out 5.69 grams of ground cinnamon| Quantity; 6; Prepare 6 cups of sliced apples| Measurement; 14; Measure out 14 grams of butter| 
 # Temperature; 450; Gather the ingredients. Preheat the oven to 450 degrees F (230 degrees C)| Untimed; None; Line your 9-inch pie dish with one pastry crust. Set other one to the side| Untimed; None; Combine 3/4 cup sugar and cinnamon in a small bowl. Add more sugar if your apples are tart| Untimed; None; Layer apple slices in the prepared pie dish, sprinkling each layer with cinnamon-sugar mixture| Untimed; None; Dot top layer with small pieces of butter. Cover with top crust| 
 # Timed; 600; Bake pie on the lowest rack of the preheated oven for 10 minutes| Timed; 1800; Reduce oven temperature to 350 degrees F (175 degrees C) and continue baking for about 30 minutes, until golden brown and filling bubbles| finish; None; None
+
+# have each recipe class contain: array of instruction classes, int counter for which step the recipe is on, (ingredient list?)
+# each instruction class has type, value, string instruction
