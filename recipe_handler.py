e54a3f2ebf3ee6108e9e243b9e7455bfc02a162b
Brandon Huang <108428038+Gafrindle@users.noreply.github.com>
2024-11-26 00:01:14 -0800

    Add logic for navigating steps

--- recipe_handler.py
+++ recipe_handler.py
@@ -6,14 +6,14 @@
 # example recipe string, based off https://www.allrecipes.com/recipe/12324/apple-pie-i/
 # Quantity; 1; Prepare two 9-inch pie crusts and one 9-inch pie dish| Measurement; 150; Measure out 150 grams of white sugar| Measurement; 5.69; Measure out 5.69 grams of ground cinnamon| Quantity; 6; Prepare 6 cups of sliced apples| Measurement; 14; Measure out 14 grams of butter| 
 # Temperature; 450; Gather the ingredients. Preheat the oven to 450 degrees F (230 degrees C)| Untimed; None; Line your 9-inch pie dish with one pastry crust. Set other one to the side| Untimed; None; Combine 3/4 cup sugar and cinnamon in a small bowl. Add more sugar if your apples are tart| Untimed; None; Layer apple slices in the prepared pie dish, sprinkling each layer with cinnamon-sugar mixture| Untimed; None; Dot top layer with small pieces of butter. Cover with top crust| 
-# Timed; 600; Bake pie on the lowest rack of the preheated oven for 10 minutes| Timed; 1800; Reduce oven temperature to 350 degrees F (175 degrees C) and continue baking for about 30 minutes, until golden brown and filling bubbles| Finish; None; None
+# Timed; 600; Bake pie on the lowest rack of the preheated oven for 10 minutes| Timed; 1800; Reduce oven temperature to 350 degrees F (175 degrees C) and continue baking for about 30 minutes, until golden brown and filling bubbles| Finish; None; Serve!
 
 # have each recipe class contain: array of instruction classes, int counter for which step the recipe is on, (ingredient list?)
 # each instruction class has type, value, string instruction
 
 # Instruction Class
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------
-class Instruction:
+class Step:
     def __init__(self, type, value, string):
         self.type = type
         self.value = value
@@ -24,35 +24,62 @@ class Instruction:
 # Recipe Class
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 class Recipe:
-    # Initialize Recipe object with list of instructions and counter = 0
+    # Initialize Recipe object with list of steps and counter = 0
     def __init__(self, recipeString: str):
-        self.instructions = self.parseInstrString(recipeString)
+        self.steps = self.parseInstrString(recipeString)
         self.stepCounter = 0
 
     def parseInstrString(self, recipeString):
-        # Prepare instruction list to later append to
-        instructions = []
+        # Prepare step list to later append to
+        steps = []
 
-        # Split recipe string into individual instruction strings
-        strInstructions = recipeString.split("| ")
+        # Split recipe string into individual step strings
+        strSteps = recipeString.split("| ")
 
-        # For each string instruction, create an instruction class with the fields from the string instruction
-        for inst in strInstructions:
-            instFields = inst.split("; ")
-            instructions.append(Instruction(instFields[0], instFields[1], instFields[2]))
+        # For each string step, create a Step class with the fields from the string step
+        for step in strSteps:
+            stepFields = step.split("; ")
+            steps.append(Step(stepFields[0], stepFields[1], stepFields[2]))
 
         # FOR DEBUGGING ----------------------------------------------------------------------------------------------------
-        # for instruction in instructions:
-        #     print(instruction.type + "," + instruction.value + "," + instruction.string + '\n')
+        # for step in steps:
+        #     print(step.type + "," + step.value + "," + step.string + '\n')
         # FOR DEBUGGING ----------------------------------------------------------------------------------------------------
 
         # Return list of instruction objects
-        return instructions
+        return steps
+    
+    # get string instruction for current step
+    def getCurrentInstruction(self):
+        return self.steps[self.stepCounter].string
+    
+    # increase step counter by 1
+    def incrementStepCounter(self):
+        if(self.stepCounter <= len(self.steps)-2):
+            self.stepCounter += 1
+            return 0
+        else:
+            return 1
+    
+    # decrease step counter by 1
+    def decrementStepCounter(self):
+        if(self.stepCounter > 0):
+            self.stepCounter -= 1
+            return 0
+        else:
+            return 1
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
+# 13 steps
+test = "Quantity; 1; Prepare two 9-inch pie crusts and one 9-inch pie dish| Measurement; 150; Measure out 150 grams of white sugar| Measurement; 5.69; Measure out 5.69 grams of ground cinnamon| Quantity; 6; Prepare 6 cups of sliced apples| Measurement; 14; Measure out 14 grams of butter| Temperature; 450; Gather the ingredients. Preheat the oven to 450 degrees F (230 degrees C)| Untimed; None; Line your 9-inch pie dish with one pastry crust. Set other one to the side| Untimed; None; Combine 3/4 cup sugar and cinnamon in a small bowl. Add more sugar if your apples are tart| Untimed; None; Layer apple slices in the prepared pie dish, sprinkling each layer with cinnamon-sugar mixture| Untimed; None; Dot top layer with small pieces of butter. Cover with top crust| Timed; 600; Bake pie on the lowest rack of the preheated oven for 10 minutes| Timed; 1800; Reduce oven temperature to 350 degrees F (175 degrees C) and continue baking for about 30 minutes, until golden brown and filling bubbles| Finish; None; Serve!"
 
-test = "Quantity; 1; Prepare two 9-inch pie crusts and one 9-inch pie dish| Measurement; 150; Measure out 150 grams of white sugar| Measurement; 5.69; Measure out 5.69 grams of ground cinnamon| Quantity; 6; Prepare 6 cups of sliced apples| Measurement; 14; Measure out 14 grams of butter| Temperature; 450; Gather the ingredients. Preheat the oven to 450 degrees F (230 degrees C)| Untimed; None; Line your 9-inch pie dish with one pastry crust. Set other one to the side| Untimed; None; Combine 3/4 cup sugar and cinnamon in a small bowl. Add more sugar if your apples are tart| Untimed; None; Layer apple slices in the prepared pie dish, sprinkling each layer with cinnamon-sugar mixture| Untimed; None; Dot top layer with small pieces of butter. Cover with top crust| Timed; 600; Bake pie on the lowest rack of the preheated oven for 10 minutes| Timed; 1800; Reduce oven temperature to 350 degrees F (175 degrees C) and continue baking for about 30 minutes, until golden brown and filling bubbles| Finish; None; None"
+recipe = Recipe(test)
+print(recipe.getCurrentInstruction())
+for x in range(12):
+    recipe.incrementStepCounter()
+    print(recipe.getCurrentInstruction())
 
-Recipe(test)
-        
+for x in range (12):
+    recipe.decrementStepCounter()
+    print(recipe.getCurrentInstruction())
