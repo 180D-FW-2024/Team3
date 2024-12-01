da047baea25d14b48e689167f3a22445b6ddd875
Brandon Huang <108428038+Gafrindle@users.noreply.github.com>
2024-12-01 00:40:22 -0800

    Set up skeleton for command_handler

---
+++ command_handler.py
@@ -0,0 +1,24 @@
+# imports here, so that this script can call the scripts it needs to
+
+def handle_command(command):
+    if(command == "next instruction"):
+        return
+    elif(command == "previous instruction"):
+        return
+    elif(command == "repeat instruction"):
+        return
+    elif(command == "list ingredients"):
+        return
+    elif(command == "current temperature"):
+        return
+    elif(command == "time remaining"):
+        return
+    elif(command == "start timer"):
+        return
+    elif(command == "stop timer"):
+        return
+    elif(command == "add ingredient"):
+        return
+    elif(command == "remove ingredient"):
+        return
+    
