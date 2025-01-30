e56229f89d8c6cadb73ef4b153d7d964e59130c6
Gafrindle <abrandofhuangs@gmail.com>
2025-01-30 00:32:04 +0000

    wip text_to_speech.py for piper

--- text_to_speech.py
+++ text_to_speech.py
@@ -2,13 +2,44 @@
 # pip install pyttsx3
 # apt-get install espeak-ng
 
-import pyttsx3
+# import pyttsx3
+
+# def tts(string):
+#     # engine = pyttsx3.init()
+
+#     # voices = engine.getProperty('voices')
+#     # for voice in voices:
+#     #     print(voice)
+
+#     # engine.setProperty('rate', 150)
+#     # engine.setProperty('voice', 'english+f3')
+
+
+#     # engine.say(string)
+#     # engine.runAndWait()
+#     # engine.stop()
+
+
+from piper.voice import PiperVoice
+import simpleaudio as sa
+import tempfile
+import os
 
 def tts(string):
-    engine = pyttsx3.init()
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_wav:
+        temp_wav_path = temp_wav.name
 
-    engine.setProperty('rate', 150)  
+    model = "en_GB-alan-medium.onnx"
+    voice = PiperVoice.load(model)
+    audio = voice.synthesize(string, temp_wav)
 
-    engine.say(string)
-    engine.runAndWait()
-    engine.stop()
+    # Play audio synchronously
+    wave_obj = sa.WaveObject.from_wave_file(temp_wav_path)
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+
+    os.remove(temp_wav_path)
+
+# Example
+tts("Hello, world!")
+
